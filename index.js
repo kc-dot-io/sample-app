d3d738eb7ad5af624e75cf7cee4a881af626d0c6
Kyle Campbell <kc@kc.io>
2021-09-13 15:32:28 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const mysql = require('mysql')
 const PORT = 3000
 const HOST = '0.0.0.0'
 
+console.log(process.env.DB_HOST)
+console.log(process.env.DB_USER)
+console.log(process.env.DB_PORT)
+
+/*
 let connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
@@ -12,10 +17,6 @@ let connection = mysql.createConnection({
     database: 'dev'
 });
 
-console.log(process.env.DB_HOST)
-console.log(process.env.DB_USER)
-console.log(process.env.DB_PORT)
-
 connection.connect(function(err) {
   if (err) {
     return console.error('MySQL connection error: ' + err.message);
@@ -23,10 +24,10 @@ connection.connect(function(err) {
 
   console.log('Connected to the MySQL server successfully');
 });
-
+*/
 const api = express()
 api.get('/', (req, res) => {
-  res.send('PROD-112\n')
+  res.send('PROD-113\n')
 })
 
 api.listen(PORT, HOST)
