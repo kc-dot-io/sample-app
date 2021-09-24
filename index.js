2c146cc6aa60f43882aa5559fb50a87556e0d117
Kyle Campbell <kc@kc.io>
2021-09-24 13:53:20 -0700

    sample app

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const mysql = require('mysql')
+
+const PORT = process.env.PORT || 3000
+const HOST = '0.0.0.0'
+
+console.log(process.env.DB_HOST)
+console.log(process.env.DB_USER)
+console.log(process.env.DB_PORT)
+
+/*
+let connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    port: process.env.DB_PORT,
+    database: 'dev'
+});
+
+connection.connect(function(err) {
+  if (err) {
+    return console.error('MySQL connection error: ' + err.message);
+  }
+
+  console.log('Connected to the MySQL server successfully');
+});
+*/
+const api = express()
+api.get('/', (req, res) => {
+  res.send('PROD-113\n')
+})
+
+api.listen(PORT, HOST)
+console.log(`Running on http://${HOST}:${PORT}`)
